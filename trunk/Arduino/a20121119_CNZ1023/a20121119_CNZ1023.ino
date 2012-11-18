f746322ac1cf484dcb016e3b62cce04aab4b71e9
kimura.kazuki.tat@gmail.com <kimura.kazuki.tat@gmail.com@f213f1a0-fb3b-2e63-8112-e633d523b266>
2012-11-18 18:00:10 +0000

    [Arduino with CNZ1023]  * Photo interrupter CNZ1023

---
+++ trunk/Arduino/a20121119_CNZ1023/a20121119_CNZ1023.ino
@@ -0,0 +1,16 @@
+ const int d_in_photo = 2;
+//const int AI4 = 4;
+
+void setup()
+{
+  //pinMode(d_in_photo, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int val = digitalRead(d_in_photo);
+  //int aval = analogRead(AI4);
+  Serial.println(val);
+  delay(10);
+}
