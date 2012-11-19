adc37e53b056d259e3b607caf82a4545ecfdd181
kimura.kazuki.tat <kimura.kazuki.tat@gmail.com>
2012-11-19 14:02:07 +0000

    [Arduino CNZ1023 interrupt]  * interrupt実装 動作確認済み

--- Arduino/a20121119_CNZ1023_interrupt/a20121119_CNZ1023_interrupt.ino
+++ Arduino/a20121119_CNZ1023_interrupt/a20121119_CNZ1023_interrupt.ino
@@ -1,20 +1,22 @@
 const int d_in_photo = 2;
 const int interrupt_photo = 0; // at D2 pin
-const int interrupt_photo = 0; // at D2 pin
 
 void photo_falling(){
-  Serial.println("falling");
+  int val = digitalRead(d_in_photo);
+  if(val == HIGH){
+    Serial.println("rising");
+  }else{
+    Serial.println("falling");
+  }
 }
 
 void setup()
 {
-  attachInterrupt(interrupt_photo, photo_falling, FALLING);
+  attachInterrupt(interrupt_photo, photo_falling, CHANGE);
   Serial.begin(9600);
 }
 
 void loop()
 {
-  int val = digitalRead(d_in_photo);
-  Serial.println(val);
   delay(10);
 }
